2b9aa20e1f4794f5a34b81eb58246446f5918684
Jen Cruz <JENCRUZ@boisestate.edu>
2021-07-07 11:18:11 -0600

    Added result plots to distance analysis.

--- distance_analysis.R
+++ distance_analysis.R
@@ -15,6 +15,7 @@ getwd()
 
 #install packages
 install.packages( "unmarked" ) #distance sampling and other population modeling
+
 #load packages
 library( tidyverse )
 library( unmarked )
@@ -46,6 +47,11 @@ dist_df <- raw_df %>%
 
 #check 
 head( dist_df ); dim( dist_df)
+# check that recorder IDs were entered correctly
+unique( dist_df$Recorder.ID )
+# two version of MSO39. Fix:
+dist_df$Recorder.ID[which(dist_df$Recorder.ID == "MSO039")] <- "MSO39"
+
 #create maxdist column, which we will modify in a loop
 dist_df$maxdist <- distcols[1]
 #loop over distance columns for each row, replacing Yes/no wiht 1/0 and
@@ -67,6 +73,8 @@ hist( dist_df$maxdist )
 ydat <- dist_df[,distcols]
 #turn into numeric values
 ydat <- apply(ydat, 2, FUN = as.numeric )
+#count records per distance
+colSums(ydat)
 #remove column names
 colnames(ydat) <- NULL
 #convert to matrix
@@ -74,7 +82,7 @@ ydat <- ydat %>% as.matrix()
 #view
 ydat[1:5,]
 class(ydat )
-as.matrix( ydat)
+
 #now extract site level predictors 
 covs <- dist_df %>% select( Habitat.type, Recorder.ID ) %>% 
       mutate( Habitat.type = as.factor(Habitat.type),
@@ -97,6 +105,115 @@ m1 <- distsamp( ~ 1 + Habitat.type + Recorder.ID ~1,
                 keyfun = "halfnorm", data = umf )
 
 m1
+
+############ results --------------------------------------
+# We start by estimating differences between recorders while keeping #
+# the habitat consistent. We choose Burnt for now as it was the habitat #
+# chosen as an intercept #
+recDet <- data.frame( Recorder.ID = unique(covs$Recorder.ID),
+                      Habitat.type = "Burnt" )
+#view
+recDet
+#predict partial relationship between recorder effects and detection
+pred.det.rec <- predict( m1, type = "det", newdata = recDet, 
+                          appendData = TRUE )
+
+# Plot:
+pred.det.rec %>%
+  # define x and y values
+  ggplot(., aes( x = Recorder.ID, y = Predicted, color = Recorder.ID ) ) + 
+  #choose preset look
+  theme_bw( base_size = 15 ) +
+  #remove legend
+  theme( legend.position = "none" ) +
+  # add labels
+  labs( x = "Recorder ID", y = "Half normal scale parameter" ) +
+  #add mean detection for each recorder
+  geom_point( size = 4 ) +
+  # add confidence intervals
+  geom_errorbar( aes(ymin = lower, ymax = upper ), 
+                 size = 1.5, width = 0.3 ) 
+
+for( i in 1:dim(pred.det.rec)[1]){
+  drange <- 0:150
+  mdet <- gxhn( x = drange, sigma = pred.det.rec$Predicted[i] )
+  ldet <- gxhn( x = drange, sigma = pred.det.rec$lower[i] )
+  hdet <- gxhn( x = drange, sigma = pred.det.rec$upper[i] )
+  a <- data.frame( drange, mdet, ldet, hdet, 
+                   Recorder = pred.det.rec$Recorder.ID[i])
+  ifelse( i == 1,
+          recdf <<- a,
+          recdf <- bind_rows( recdf, a) )
+}
+#check
+head( recdf )
+#plot all 
+recdf %>%  ggplot(., aes( x = drange, y = mdet, color = Recorder ) ) + 
+  #choose preset look
+  theme_bw( base_size = 15 ) + theme( legend.position = "top" ) +
+  # add labels
+  labs( x = "Distance (m)", y = "Predicted detection" ) +
+  # add band of confidence intervals
+  geom_smooth( aes(ymin = ldet, ymax = hdet, fill = Recorder ), 
+               stat = "identity",
+               size = 1.5, alpha = 0.2 ) +
+  # add mean line on top
+  geom_line( size = 2 ) 
+####----------------------------------------
+# Repeat for habitat type, keeping recorder consistent. We stick to a 
+# good recorder for now, but can also produce another with a poor one.
+habDet <- data.frame( Recorder.ID = "MSO39",
+                      Habitat.type = unique(covs$Habitat.type) )
+#view
+habDet
+#predict partial relationship between recorder effects and detection
+pred.det.hab <- predict( m1, type = "det", newdata = habDet, 
+                          appendData = TRUE )
+
+# Plot:
+pred.det.hab %>%
+  # define x and y values
+  ggplot(., aes( x = Habitat.type, y = Predicted, color = Habitat.type ) ) + 
+  #choose preset look
+  theme_bw( base_size = 15 ) +
+  #remove legend
+  theme( legend.position = "none" ) +
+  # add labels
+  labs( x = "Habitat type", y = "Half normal scale parameter (m)" ) +
+  #add mean detection for each recorder
+  geom_point( size = 4 ) +
+  # add confidence intervals
+  geom_errorbar( aes(ymin = lower, ymax = upper ), 
+                 size = 1.5, width = 0.3 ) 
+
+# We now use the sigma estimates to plot the probability detection function
+# We loop through each habitat to estimate the detection function
+for( i in 1:dim(pred.det.hab)[1]){
+  drange <- 0:150
+  mdet <- gxhn( x = drange, sigma = pred.det.hab$Predicted[i] )
+  ldet <- gxhn( x = drange, sigma = pred.det.hab$lower[i] )
+  hdet <- gxhn( x = drange, sigma = pred.det.hab$upper[i] )
+  a <- data.frame( drange, mdet, ldet, hdet, 
+                  habitat = pred.det.hab$Habitat.type[i])
+  ifelse( i == 1,
+        habdf <<- a,
+        habdf <- bind_rows( habdf, a) )
+}
+#check
+head( habdf )
+#plot all 
+habdf %>%  ggplot(., aes( x = drange, y = mdet, color = habitat ) ) + 
+  #choose preset look
+  theme_bw( base_size = 15 ) + theme( legend.position = "top" ) +
+  # add labels
+  labs( x = "Distance (m)", y = "Predicted detection" ) +
+  # add band of confidence intervals
+  geom_smooth( aes(ymin = ldet, ymax = hdet, fill = habitat ), 
+               stat = "identity",
+               size = 1.5, alpha = 0.2 ) +
+  # add mean line on top
+  geom_line( size = 2 ) 
+
 #############################################################################
 # Saving relevant objects and data ---------------------------------
 #save workspace in case we need to make changes
